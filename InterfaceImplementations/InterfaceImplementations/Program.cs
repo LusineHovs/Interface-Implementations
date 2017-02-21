8deb049c7d94d8af6e3a35fc74260da338ecf84a
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-02-21 12:36:14 +0400

    Some additions

---
+++ InterfaceImplementations/InterfaceImplementations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceImplementations
+{
+    interface IExample
+    {
+        int Method(int e);
+    }
+    // IExample interface explicit implementation
+    class A : IExample
+    {
+        int IExample.Method(int a)
+        {
+            return a * a;
+        }
+    }
+
+    // IExample interface implicit implementation
+    class B : IExample
+    {
+        public int Method(int e)
+        {
+            return e*e;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In case of explicit implementation
+            A a = new A();
+            ((IExample)a).Method(10);
+
+            //In case of implicit implementation
+            B b = new B();
+            b.Method(12);
+
+        }
+    }
+}
